915be2d2b630151eab930ffd4caf5a103955797b
Jose Herrera <jherrera892016@gmail.com>
2021-11-23 23:59:13 -0500

    Cambio Titulo y Modulo Eliminar Ok

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 
-import { isEmpty } from 'lodash';
+import { isEmpty,size } from 'lodash';
 import React, {useState} from 'react';
 import shortid from 'shortid';
 
@@ -28,6 +28,11 @@ e.preventDefault();
  
  setTask("");
 }
+const deleteTask =(id)=>{
+  const filteredTask=tasks.filter(task=>task.id!==id)
+  setTasks(filteredTask);
+}
+
   return (
     <div className="container mt-5">
       <h1>Actividades</h1>
@@ -35,29 +40,39 @@ e.preventDefault();
       <div className="row">
         <div className="col-8">
           <h4 className="text-center">Lista De Actividades</h4>
-          <ul className="list-group">
-        { 
-        tasks.map((task) => (
-          <li className="list-group-item" key={task.id}>
-            <span className="lead">{task.name}</span>
-            <button 
-              className="btn btn-danger btn-sm float-right mx-2"
-              
-            >
-              Eliminar
-            </button>
-            <button 
-              className="btn btn-warning btn-sm float-right"
-              
-            >
-              Editar
-            </button>
-          </li>
+      
+      {    
+        size(tasks)===0?(
+          <h5 className="text-center">Aun No Hay Actividades Programadas</h5>
+        ):(
 
-        ))
-          }
+        <ul className="list-group">
+          { 
+            tasks.map((task) => (
+              <li className="list-group-item" key={task.id}>
+                <span className="lead">{task.name}</span>
+                <button 
+                  className="btn btn-danger btn-sm float-right mx-2"
+                onClick={()=>deleteTask(task.id)}
+                >
+                  Eliminar
+                </button>
+                <button 
+                  className="btn btn-warning btn-sm float-right"
+                  
+                >
+                  Editar
+                </button>
+              </li>
 
-          </ul>
+            ))
+              }
+
+        </ul>
+        )
+
+        }
+
         </div>
         <div className="col-4">
           <h4>Formulario</h4>
