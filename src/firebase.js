37d33dd17cc0450504175b1b00cc27f3bf2e74f8
Jose Herrera <66761042+featlast@users.noreply.github.com>
2021-11-30 15:40:23 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,7 @@ import 'firebase/compat/firestore';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAuuQ8Dn6beZZQ6e6vxmLeVyPa83xuidFg",
-    authDomain: "crud-2fa49.firebaseapp.com",
-    projectId: "crud-2fa49",
-    storageBucket: "crud-2fa49.appspot.com",
-    messagingSenderId: "954897910850",
-    appId: "1:954897910850:web:313aecdfe23c73d212f1db"
+    
   }
 
   
